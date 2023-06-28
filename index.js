a3e61e18c2d91332ac6035afb68072e88dce2f07
Francis Reyes <reyesfrancisp@gmail.com>
2023-06-28 17:09:10 -0400

    Can generate an svg file

--- index.js
+++ index.js
@@ -1,55 +1,81 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
-const generateShape = require("./lib/shapes.js");
 const colorValidate = require("validate-color").default;
-//const colorConvert = require("color-convert");
-
+const fs = require("fs");
+const Shape = require("./lib/shapes.js");
 const userPrompt = inquirer.createPromptModule();
 
 const questions = [
-    {
-        name: "text",
-        message: "What text do you want?",
-        validate: function (input) {
-            if (input.length <= 3) {
-              return true;
-            }
-            return 'Please enter three letters or less.';
-          },
+  {
+    name: "text",
+    message: "What text do you want?",
+    validate: function (input) {
+      if (input.length <= 3) {
+        return true;
+      }
+      return "Please enter three letters or less.";
     },
-    {
-        name: "textcolor",
-        message: "What color do you want for the text?",
-        validate: function (input) {
-          if (colorValidate(input)) {
-            return true;
-          } else
-          return 'Please enter a valid color or hex value (e.g. #000000).';
-        }
+  },
+  {
+    name: "textcolor",
+    message: "What color do you want for the text?",
+    validate: function (input) {
+      if (colorValidate(input)) {
+        return true;
+      } else {
+        return "Please enter a valid color or hex value (e.g. #000000).";
+      }
     },
-    {
-        name: "shape",
-        message: "What shape do you want?",
-        type: "list",
-        choices: [
-            "Circle",
-            "Triangle",
-            "Square"
-        ],
-
+  },
+  {
+    name: "shape",
+    message: "What shape do you want?",
+    type: "list",
+    choices: [
+      "Circle",
+      "Triangle",
+      "Square"
+    ],
+  },
+  {
+    name: "shapecolor",
+    message: "What color do you want for the shape?",
+    validate: function (input) {
+      if (colorValidate(input)) {
+        return true;
+      } else {
+        return "Please enter a valid color or hex value (e.g. #000000).";
+      }
     },
-    {
-        name: "shapecolor",
-        message: "What color do you want for the shape?",
-        validate: function (input) {
-            if (colorValidate(input)) {
-              return true;
-            } else
-            return 'Please enter a valid color or hex value (e.g. #000000).';
-          }
-    }
-    
+  }
 ];
 
+userPrompt(questions).then((answers) => {
+  // Create an instance of the Shape class based on the selected shape
+   const shape = new Shape(answers.text, answers.textcolor, answers.shapecolor);
 
-userPrompt(questions).then((answers) => console.log(answers));
+  // Generate the SVG based on the provided answers
+  let svg;
+  switch (answers.shape) {
+    case "Circle":
+      svg = shape.makeCircle();
+      break;
+    case "Triangle":
+      svg = shape.makeTriangle();
+      break;
+    case "Square":
+      svg = shape.makeSquare();
+      break;
+    default:
+      console.error("Invalid shape");
+      return;
+  }
+
+  // Save the SVG to a file
+  fs.writeFile("logo.svg", svg, (err) => {
+    if (err) {
+      console.error("Error saving SVG:", err);
+    } else {
+      console.log("SVG saved to logo.svg");
+    }
+  });
+});
